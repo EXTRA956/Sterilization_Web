dedd357cf29866269aea359b026b2bf1b6212534
EXTRA956 <daniel.arato12@gmail.com>
2026-09-16 00:51:30 +0100

    added database insert functions for comments and instruments

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
-from models import EnumCommentType, EnumPackaging, EnumSetState
+from models import EnumCommentType, EnumPackaging, EnumSetState, Comment, Instrument
 
 def get_connection() -> sqlite3.Connection:
-    return sqlite3.connect("sterilization.db")
+    connection = sqlite3.connect("sterilization.db")
+    connection.execute("PRAGMA foreign_keys = ON")
+    return connection
 
 def seed_enum_table(cursor: sqlite3.Cursor, table_name: str, enum_class: EnumCommentType | EnumPackaging | EnumSetState) -> None:
     for member in enum_class:
@@ -15,3 +17,31 @@ def seed_all_enum_tables(cursor: sqlite3.Cursor) -> None:
     seed_enum_table(cursor, "enum_comment_type", EnumCommentType)
     seed_enum_table(cursor, "enum_packaging", EnumPackaging)
     seed_enum_table(cursor, "enum_set_state", EnumSetState)
+
+def insert_comment(cursor: sqlite3.Cursor, comment: Comment) -> int:
+    cursor.execute(
+        f"INSERT INTO comments (content, user, date_time, type_id) VALUES (?, ?, ?, ?)"
+        (comment.content, comment.user, comment.date_time.isoformat(), comment.type.value)
+    )
+
+    comment_id = cursor.lastrowid
+
+    # Intentional bypass of frozen=true. data_base_id is ease of use only, doesnt need immutability for an audit.
+    object.__setattr__(comment, "data_base_id", comment_id)
+
+    return comment_id
+
+def insert_instrument(cursor: sqlite3.Cursor, instrument: Instrument, set_id : int) -> int:
+    comment_id = None
+    if instrument.comment is not None:
+        comment_id = insert_comment(cursor, instrument.comment)
+
+    cursor.execute(
+        f"INSERT INTO instruments (name, remaining_uses, set_id, comment_id) VALUES (?, ?, ?, ?)",
+        (instrument.name, instrument.remaining_uses, set_id, comment_id)
+    )
+
+    instrument_id = cursor.lastrowid
+    instrument.data_base_id = instrument_id
+    
+    return instrument_id
