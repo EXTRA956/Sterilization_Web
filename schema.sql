9563562af9b716d96f3374769add27012cb36c42
EXTRA956 <daniel.arato12@gmail.com>
2026-09-15 02:37:51 +0100

    Created draft Schema.SQL: enum tables, and object tabes to reflect models.py

---
+++ schema.sql
@@ -0,0 +1,35 @@
+-- Enums
+CREATE TABLE enum_comment_types (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE enum_packaging (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE enum_set_state (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+-- Data Tables
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
+    type_id INTEGER NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES enum_comment_types(id),
+    content TEXT NOT NULL,
+    author TEXT NOT NULL,
+    date_time TEXT NOT NULL
+);
+
+CREATE TABLE sets (
+    serial_number INTEGER PRIMARY KEY,
+    active_comment_id INTEGER,
+    FOREIGN KEY (active_comment_id) REFERENCES comments(id),
+    packaging_id INTEGER,
+    FOREIGN KEY (packaging_id) REFERENCES enum_packaging(id),
+    name TEXT NOT NULL,
+    customer TEXT NOT NULL
+);
