61627517de654231e605d8ef653835145620fc0a
EXTRA956 <daniel.arato12@gmail.com>
2026-09-15 03:01:15 +0100

    fixed crash; data collums after foreign keys casue a crash.. all foreign keys have to be at the end of CREATE TABLE

--- schema.sql
+++ schema.sql
@@ -17,40 +17,40 @@ CREATE TABLE enum_set_state (
 -- Data Tables
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY,
-    type_id INTEGER NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES enum_comment_types(id),
     content TEXT NOT NULL,
     author TEXT NOT NULL,
-    date_time TEXT NOT NULL
+    date_time TEXT NOT NULL,
+    type_id INTEGER NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES enum_comment_types(id)
 );
 
 CREATE TABLE sets (
     serial_number INTEGER PRIMARY KEY,
-    active_comment_id INTEGER,
-    FOREIGN KEY (active_comment_id) REFERENCES comments(id),
-    packaging_id INTEGER,
-    FOREIGN KEY (packaging_id) REFERENCES enum_packaging(id),
     name TEXT NOT NULL,
-    customer TEXT NOT NULL
+    customer TEXT NOT NULL,
+    active_comment_id INTEGER,
+    packaging_id INTEGER,
+    FOREIGN KEY (active_comment_id) REFERENCES comments(id),
+    FOREIGN KEY (packaging_id) REFERENCES enum_packaging(id)
 );
 
 CREATE TABLE set_states (
     id INTEGER PRIMARY KEY,
+    user TEXT NOT NULL,
+    date_time TEXT NOT NULL,
+    state_id INTEGER,
+    comment_id INTEGER,
     set_id INTEGER NOT NULL,
     FOREIGN KEY (set_id) REFERENCES sets(serial_number),
-    state_id INTEGER,
     FOREIGN KEY (state_id) REFERENCES enum_set_state(id),
-    comment_id INTEGER,
-    FOREIGN KEY (comment_id) REFERENCES comments(id),
-    user TEXT NOT NULL,
-    date TEXT NOT NULL
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
 );
 
 CREATE TABLE instruments (
     id INTEGER PRIMARY KEY,
+    remaining_uses INTEGER,
     set_id INTEGER NOT NULL,
-    FOREIGN KEY (set_id) REFERENCES sets(serial_number),
     comment_id INTEGER,
-    FOREIGN KEY (comment_id) REFERENCES comments(id),
-    remaining_uses INTEGER
+    FOREIGN KEY (set_id) REFERENCES sets(serial_number),
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
 );
